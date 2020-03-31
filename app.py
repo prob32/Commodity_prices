67eae740e2c322e0c0b2dd57de3cc5a55799b705
prob32 <patrick.s.robinson@maine.edu>
2020-03-31 18:48:51 -0400

    Update app.py

--- app.py
+++ app.py
@@ -28,20 +28,45 @@ app.layout = html.Div(children=[
     html.H1(myheading1,
             style = {'color': colors['text']}
 ),
+    #Filing Status
+    html.Label('Filing status'),
+    dcc.Dropdown(
+        id='filing_status',
+        options=[{'label': "Single", 'value': 1},
+                 {'label': "Married", 'value': 2}], value= 1),
+
     #################### Salary inputs
     html.Label('Enter your gross income on your most recent tax filing 2018 or 2019'),
-    dcc.Input(id='Salary',  type='number'),
+    dcc.Input(id='salary',  type='number'),
 
 
 
     ######################## Dependents inputs
     html.Label('Enter number of dependents you claimed'),
-    dcc.Input(id='Dependents', type='number'),
+    dcc.Input(id='dependents', type='number',value=0),
 
-])
-def update_output_div(input_value):
-    return 'You\'ve entered "{}"'.format(input_value)
+    ######## output functions
+    html.H4(id='my-div3', style={'color': colors['text']}),], style = {'columnCount': 1}
+)
 
+########## callbacks
+@app.callback(
+    Output(component_id='my-div3', component_property='children'),
+    [Input(component_id='filing_status', component_property='value'),
+     Input(component_id='salary', component_property='value'),
+     Input(component_id='dependents', component_property='value')])
+
+####### tax logic function
+def update_output_div(filing_status,salary,dependents):
+    if salary <= 75000*filing_status:
+        x = 1200*filing_status
+    else:
+        difference = (salary)-(75000*filing_status)
+        depreciate_amount = int(difference/100)*5
+        depreciate_amount = min(1200*filing_status, depreciate_amount)
+        x =  1200*filing_status - depreciate_amount
+    y = x + 500*dependents
+    return y
 ############ Deploy
 if __name__ == '__main__':
     app.run_server()
