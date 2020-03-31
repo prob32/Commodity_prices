e3e120561fde39fe8b7c07b72e97d1d4afddb500
prob32 <patrick.s.robinson@maine.edu>
2020-03-31 16:08:18 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,21 +7,10 @@ from collections import deque, Counter
 
 ########### Define your variables ######
 
-myheading1='Try out a palindrome here!'
-initial_value='A nut for a jar of tuna'
-longtext='''
-        _Suggestions you might try:_
-        * A man, a plan, a canal: Panama!
-        * Go hang a salami I'm a lasanga hog
-        * God! Nate bit a Tibetan dog!
-        '''
-tabtitle = 'racecar'
-sourceurl = 'https://www.grammarly.com/blog/16-surprisingly-funny-palindromes/'
-githublink = 'https://github.com/austinlasseter/dash-simple-callback'
+myheading1='COVID-19 Tax rebate'
+tabtitle = 'COVID-19 Income tax calculator'
 
-########### Define a function for your callback:
-def my_function(letters):
-    return(letters[::-1])
+
 
 ########### Initiate the app
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -34,13 +23,7 @@ app.title=tabtitle
 app.layout = html.Div(children=[
     html.H1(myheading1),
     html.Div(children=[dcc.Markdown(longtext)]),
-    dcc.Input(id='my-id', value=initial_value, type='text'),
-    html.Div(id='my-div'),
-    html.Br(),
-    html.A('Code on Github', href=githublink),
-    html.Br(),
-    html.A("Data Source", href=sourceurl),
-    ]
+    dcc.Input(id='my-id', value=initial_value, type='number'),]
 )
 
 
@@ -50,8 +33,7 @@ app.layout = html.Div(children=[
     [Input(component_id='my-id', component_property='value')]
 )
 def update_output_div(input_value):
-    palin=my_function(input_value)
-    return f"You've entered '{input_value}', and your output is '{palin}'"
+    return 'You\'ve entered "{}"'.format(input_value)
 
 ############ Deploy
 if __name__ == '__main__':
